c6de69a2ec68060de6970755960c5e0cff763a4f
Khunhour Morm <khonhourmorm@gmail.com>
2021-12-24 02:04:21 +0900

    wrote api fetch data function and useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 export default function App() {
+	const [menItems, setMenItems] = useState({});
+	const [womenItems, setWomenItems] = useState({});
+	const [wishList, setWishList] = useState({});
+	const [cart, setCard] = useState({});
+
+	const fetchMenItems = async () => {
+		const data = await fetch(
+			"https://fakestoreapi.com/products/category/men's%20clothing"
+		);
+		const formattedData = await data.json();
+		setMenItems(formattedData);
+	};
+
+	const fetchWomenItems = async () => {
+		const data = await fetch(
+			"https://fakestoreapi.com/products/category/women's%20clothing"
+		);
+		const formattedData = await data.json();
+		setWomenItems(formattedData);
+	};
+
 	return (
 		<div>
-			<Header />
 			<h1>App</h1>
+			<Header />
+			<Main />
 		</div>
 	);
 }
