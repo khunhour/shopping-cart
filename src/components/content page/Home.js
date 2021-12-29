44991802bd0677fc08afdbb4faa36068998fdd85
Khunhour Morm <khonhourmorm@gmail.com>
2021-12-29 17:17:45 +0900

    modify styling to cardcss with hovevr

--- src/components/content page/Home.js
+++ src/components/content page/Home.js
@@ -1,5 +1,15 @@
 import React from "react";
 
 export default function Home() {
-	return <div>Home</div>;
+	return (
+		<div>
+			<h1>Are you a developer?</h1>
+			<h1>
+				Are you looking for clothing other than that one T-shirt you got
+				from a start-up event from 5 years ago?
+			</h1>
+			<h1>Look no further.</h1>
+			<h1>dev:store's products are made for developers like you!</h1>
+		</div>
+	);
 }
