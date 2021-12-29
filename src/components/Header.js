1a257a178a9f92e99151e1c3828c2242ff667892
Khunhour Morm <khonhourmorm@gmail.com>
2021-12-29 20:18:28 +0900

    added icon to header  and added get total quantities function to Cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-
+import { MdShoppingCart } from "react-icons/md";
+import { allItemsContext } from "../App";
+import { MdFavorite, MdHome } from "react-icons/md";
 export default function Header() {
+	const cartItems = useContext(allItemsContext).cartItems;
+	const totalItems = getTotalQuantities(cartItems);
 	return (
 		<header>
 			<div className="upper-nav">
 				<h3>{"dev : store"}</h3>
 				<div className="upper-nav-links">
-					<Link to="/">Home</Link>
-					<Link to="/wishlist">WishList</Link>
-					<Link to="/checkout">Checkout</Link>
+					<Link to="/">
+						<MdHome /> Home
+					</Link>
+					<Link to="/wishlist">
+						<MdFavorite /> WishList
+					</Link>
+					<Link to="/checkout">
+						<MdShoppingCart color="white" /> Cart{`(${totalItems})`}
+					</Link>
 				</div>
 			</div>
 			<nav className="lower-nav">
@@ -18,4 +28,11 @@ export default function Header() {
 			</nav>
 		</header>
 	);
+}
+
+function getTotalQuantities(items) {
+	const total = items.reduce((sum, item) => {
+		return (sum += item.quantity);
+	}, 0);
+	return total;
 }
