c845482077b5b9b49f22e2e8c5a50202585b3b68
Khunhour Morm <khonhourmorm@gmail.com>
2021-12-29 20:39:16 +0900

    create slider data for image slider

---
+++ src/components/Image Slider/SliderData.js
@@ -0,0 +1,18 @@
+export const SliderData = [
+	{
+		image: "https://images.unsplash.com/photo-1638913662252-70efce1e60a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxzZWFyY2h8MXx8ZGV2ZWxvcGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+		text: "Are you a developer?",
+	},
+	{
+		image: "https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8dHNoaXJ0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+		text: "Are you looking for clothing other than that one T-shirt you got from a start-up event from 5 years ago?",
+	},
+	{
+		image: "https://images.unsplash.com/photo-1515598379512-b60132823a9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bG9va2luZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+		text: "Look no further.",
+	},
+	{
+		image: "https://images.unsplash.com/photo-1556905055-8f358a7a47b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2xvdGhlc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+		text: "dev:store's products are made for developers like you!",
+	},
+];
